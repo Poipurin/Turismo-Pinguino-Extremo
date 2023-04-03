f018044a779d2cc5ffd97a6c4b410244c56b3130
Poipurin <b.pinorojo@gmail.com>
2023-04-03 15:09:51 -0400

    se actualiza DatosCliente.java

--- src/modelo/DatosCliente.java
+++ src/modelo/DatosCliente.java
@@ -3,4 +3,28 @@ package modelo;
 public class DatosCliente {
     private String nombre;
     private String rut;
+
+    public DatosCliente(String nombre, String rut) {
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+
+    public DatosCliente() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
 }
